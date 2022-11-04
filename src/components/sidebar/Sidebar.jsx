1fded12ff2462343f4ea69f6a311787c2bb4cf9f
Maneesh Busi <maneesh.busi@gmail.com>
2022-11-04 22:36:06 +0530

    working version 1.0

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./sidebar.scss";
 
 import DashboardIcon from "@mui/icons-material/Dashboard";
@@ -14,11 +14,18 @@ import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSyst
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
+import { Link } from "react-router-dom";
+import { DarkModeContext } from "../../context/darkModeContext";
+
 const Sidebar = () => {
+  const { darkMode, dispatch } = useContext(DarkModeContext);
+
   return (
     <div className="sidebar">
       <div className="top">
-        <span className="logo">sudoadmin</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">sudoadmin</span>
+        </Link>
       </div>
       <hr />
       <div className="center">
@@ -29,16 +36,18 @@ const Sidebar = () => {
             <span>Dashboard</span>
           </li>
           <p className="title">LISTS</p>
-
-          <li>
-            <PersonOutlineIcon className="icon" />
-            <span>Users</span>
-          </li>
-
-          <li>
-            <StoreIcon className="icon" />
-            <span>Products</span>
-          </li>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonOutlineIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <StoreIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
 
           <li>
             <CreditCardIcon className="icon" />
@@ -82,8 +91,14 @@ const Sidebar = () => {
         </ul>
       </div>
       <div className="bottom">
-        <div className="colorOption"></div>
-        <div className="colorOption"></div>
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "LIGHT" })}
+        ></div>
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "DARK" })}
+        ></div>
       </div>
     </div>
   );
